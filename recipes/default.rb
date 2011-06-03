2b4a705dc2cb06e1b3149131fc2f0d08277b5a44
Carlo Zottmann <carlo@municode.de>
2011-06-03 17:39:19 +0200

    Removed non-existing dependency.  DRY'ing.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,34 +1,32 @@
 include_recipe "apt"
 include_recipe "build-essential"
 
-%w( wget zlib1g-dev libssl-dev libffi-dev libxslt-dev libxml2-dev libncurses5-dev libreadline5-dev ).each do |pkg|
-  package pkg do
-    action :install
-  end
+ruby_installed_check = "ruby -v | grep #{ node[:ruby][:version].gsub( '-', '' ) }"
+
+%w( wget zlib1g-dev libssl-dev libffi-dev libxml2-dev libncurses5-dev libreadline5-dev ).each do |pkg|
+  package pkg { action :install }
 end
 
-execute "get, unpack #{ node[:ruby][:version] }" do
+execute "get & unpack #{ node[:ruby][:version] }" do
   user "root"
   command "cd /usr/src && wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-#{ node[:ruby][:version] }.tar.bz2 && tar xjf ruby-#{ node[:ruby][:version] }.tar.bz2 && cd ruby-#{ node[:ruby][:version] }"
-  not_if "ruby -v | grep #{ node[:ruby][:version].gsub( '-', '' ) }"
+  not_if ruby_installed_check
 end
 
 execute "configure & make #{ node[:ruby][:version] }" do
   user "root"
   command "cd /usr/src/ruby-#{ node[:ruby][:version] } && ./configure && make && make install"
-  not_if "ruby -v | grep #{ node[:ruby][:version].gsub( '-', '' ) }"
+  not_if ruby_installed_check
 end
 
-execute "configure & make #{ node[:ruby][:version] }'s openssl support" do
-  user "root"
-  command "cd /usr/src/ruby-#{ node[:ruby][:version] }/ext/openssl/ && ruby extconf.rb && make && make install"
-  not_if "ruby -v | grep #{ node[:ruby][:version].gsub( '-', '' ) }"
+%w( openssl readline ).each do |ext|
+  execute "configure & make #{ node[:ruby][:version] } #{ext} support" do
+    user "root"
+    command "cd /usr/src/ruby-#{ node[:ruby][:version] }/ext/#{ext}/ && ruby extconf.rb && make && make install"
+    not_if ruby_installed_check
+  end
 end
 
-gem_package "ohai" do
-  action :install
-end
-
-gem_package "chef" do
-  action :install
+%w( ohai chef ).each do |g|
+  gem_package g { action :install }
 end
