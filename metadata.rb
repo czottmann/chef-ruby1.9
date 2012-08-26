692f2e6cb0944f46e1f03300409a31c6fd29cb42
Hilary Holz <hilary@hholz.com>
2012-08-26 11:56:44 -0700

    edit metadata.rb and README to reduce confusion for chef noobs (like me!)
    I changed the references to the default recipe in metadata.rb and README.mdown to
    be consistent with actual usage :)

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
 maintainer        "Carlo Zottmann"
 maintainer_email  "carlo@zottmann.org"
 license           "WTFPL"
-description       "Installs Ruby 1.9.2 from source"
+description       "Installs Ruby 1.9.x from source"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.mdown'))
 version           "1.0"
 supports          "ubuntu"
 
-%w( apt build-essential ).each do |d|
-  depends d
+%w( apt build-essential ).each do |cb|
+  depends cb
 end
 
-recipe            "ruby-1.9.2", "Installs Ruby 1.9.2 from source."
+recipe "chef-ruby1.9", "Installs Ruby 1.9.x (default: 1.9.2) from source."
